6a2320cfb7d0292b649e94a7b357759741a8eaf8
Leonhard Breuer <git@leonhardbreuer.de>
2025-07-13 13:27:09 +0200

    feat: add more verbose logging

--- apis/flags.go
+++ apis/flags.go
@@ -14,29 +14,43 @@ type FlaglyContext struct {
 
 func GetAllFlags(c *gin.Context) {
 	logger := flagly.GetLogger(c)
-	logger.Info("Fetching all flags")
-	c.JSON(200, flagly.Store.Flags)
+	logger.Info("Attempting to fetch all flags")
+
+	flags := flagly.Store.Flags
+	logger.Infow("Successfully fetched all flags",
+		"count", len(flags),
+	)
+	c.JSON(200, flags)
 }
 
 func GetFlag(c *gin.Context) {
 	logger := flagly.GetLogger(c)
-	flag_key := c.Param("flag")
-	logger.Infow("Fetching a single flag",
-		"flag_key", flag_key,
+	flagKey := c.Param("flag")
+
+	logger.Infow("Attempting to fetch a single flag",
+		"flag_key", flagKey,
 	)
+
 	var selectedFlag *flagly.Flag
 	for _, f := range flagly.Store.Flags {
-		if f.Key == flag_key {
+		if f.Key == flagKey {
 			selectedFlag = &f
 			break
 		}
 	}
+
 	if selectedFlag == nil {
-		msg := fmt.Sprintf("flag '%s' not found", flag_key)
-		logger.Warn(msg)
+		msg := fmt.Sprintf("flag '%s' not found", flagKey)
+		logger.Warnw(msg,
+			"flag_key", flagKey,
+		)
 		c.Error(errors.New("flag not found"))
 		return
 	}
+
+	logger.Infow("Successfully found flag",
+		"flag_key", flagKey,
+	)
 	c.JSON(200, selectedFlag)
 }
 
@@ -49,29 +63,45 @@ type PostEvaluateFlagDTO struct {
 func PostEvaluateFlag(c *gin.Context) {
 	logger := flagly.GetLogger(c)
 	var data PostEvaluateFlagDTO
+
 	if err := c.ShouldBind(&data); err != nil {
-		logger.Info(err.Error())
+		logger.Errorw("Failed to bind request data",
+			"error", err.Error(),
+		)
 		c.Error(err)
 		return
 	}
+
+	logger.Infow("Attempting to evaluate a flag",
+		"flag_key", data.Flag,
+		"user_id", data.User.ID,
+		"environment", data.Environment,
+	)
+
 	var flag *flagly.Flag
 	for _, f := range flagly.Store.Flags {
-		logger.Info(f)
 		if f.Key == data.Flag {
 			flag = &f
+			break
 		}
 	}
+
 	if flag == nil {
-		c.Error(errors.New(("flag not found")))
+		logger.Warnw("Evaluation failed because flag was not found",
+			"flag_key", data.Flag,
+		)
+		c.Error(errors.New("flag not found"))
 		return
 	}
 
 	result := flagly.EvaluateFlag(*flag, data.User, data.Environment)
 
-	if !result {
-		c.JSON(200, gin.H{"enabled": false})
-		return
-	}
-	c.JSON(200, gin.H{"enabled": true})
+	logger.Infow("Flag evaluation completed",
+		"flag_key", flag.Key,
+		"user_id", data.User.ID,
+		"environment", data.Environment,
+		"result", result,
+	)
 
+	c.JSON(200, gin.H{"enabled": result})
 }
